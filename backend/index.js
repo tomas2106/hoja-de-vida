d90b66ef92a4097d462e8368e822bfd1820a2489
tomas2106 <jeison.tomas21@gmail.com>
2026-06-22 16:36:33 -0500

    reorganizacion de carpetas del proyecto

--- backend/index.js
+++ backend/index.js
@@ -9,14 +9,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Conexión PostgreSQL
+
 const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-});
+}); 
 
 // Ruta prueba
 app.get("/", (req, res) => {
